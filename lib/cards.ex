d05771f3f6e0b789f83a95c6b2b4d064987333a7
castrodrigo <castrodrigo@yahoo.com.br>
2019-08-04 17:02:55 +0200

    Update create Deck to involve more cards and suits

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,8 +2,12 @@ defmodule Cards do
   @moduledoc """
     Provides a set of operations to create and handle a deck of cards
   """
-
   def create_deck do
-    ["Ace", "Two", "Three", "Four", "Five"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 end
