0089aba8e85308134ad43803ce4f4aef35b5c877
Rodrigo de Castro <castrodrigo@yahoo.com.br>
2019-08-04 17:36:42 +0200

    Feature improve function dos (#3)
    * Add create_dock doc
    
    * Add deal doc
    
    * Add actual example code of how deal works
    
    * Add pieces of doc to other methods

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,10 @@
 defmodule Cards do
   @moduledoc """
-    Provides a set of operations to create and handle a deck of cards
+    Provides a set of operations to create and handle a deck of cards.
+  """
+
+  @doc """
+    Returns a list of strings mirroring a real life deck, with a set of individual cards and their suits.
   """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
@@ -11,23 +15,50 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Shuffles all the cards in the received `deck` param, returning a new list with different order than the original.
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Verfies if one received `card` is inside a specified `deck`
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Splits a deck into two subgroups:
+    - The first one group of data is referring to a hand that the user will receive, with an specified number of cards.
+    - The second group consists of all the other cards that are remaining in the deck.
+    The `hand_size` argument denotes how many cards should be given to the user.
+
+  ## Examples
+
+      iex > deck = Cards.create_deck()
+      iex > {hand, rest_of_deck} = Cards.deal(deck, 1)
+      iex > hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
+  @doc """
+    Saves a generated `deck` to the OS FileSystem, using :erlang piece of code.
+    A path can be passed in with the `filename` param.
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write!(filename, binary)
   end
 
+  @doc """
+    Loads a file into module using readable format, after specifin a path in the `filename`.
+  """
   def load(filename) do
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term(binary)
@@ -35,6 +66,9 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Concats Cards.create_deck(), Cards.shuffle(deck) and Cards.deal(deck, hand_size) using Pipe operator, making one call only.
+  """
   def create_hand(hand_size) do
     Cards.create_deck()
     |> Cards.shuffle()
